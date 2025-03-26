4c813fa60fb98a75a1333da415d9bdd7c1415470
marielleaguirre <mariellemaebaguirre@iskolarngbayan.pup.edu.ph>
2025-03-27 06:08:01 +0800

    Asked the user to input a string and initialized an empty string for the lowercase characters

--- convert_to_lowercase.py
+++ convert_to_lowercase.py
@@ -2,6 +2,9 @@
 # Create a program that do the same functionality without using lower() function.
 
 # ask user for string input and initialize an empty string, lower_case_string
+string_input = input("Enter a string: ")
+lower_case_string = ""
+
 # using for loop to loop through each characters in the string
 # if a character is an uppercase character, convert to lowercase by adding 32 to its ASCII value
 # print the string input in lowercase
