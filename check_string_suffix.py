50703bfde1cb34d2d039d7f9eeee8b9bda321fad
marielleaguirre <mariellemaebaguirre@iskolarngbayan.pup.edu.ph>
2025-03-27 06:40:11 +0800

    Asked user for a string input and the suffix they want to check

--- check_string_suffix.py
+++ check_string_suffix.py
@@ -2,5 +2,8 @@
 # Create a program that do the same functionality without using endswith() function.
 
 # ask user for string input and a suffix to check
+string_input = input("Enter a string: ")
+suffix_to_check = input("Enter the suffix you wish to check: ")
+
 # compare the end of the string input with the suffix input
 # print True if string ends with the suffix input, otherwise print False
