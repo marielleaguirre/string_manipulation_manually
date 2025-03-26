17c6c33ca3eff3c136b41151b8a1a3e198b84105
marielleaguirre <mariellemaebaguirre@iskolarngbayan.pup.edu.ph>
2025-03-27 07:24:14 +0800

    Used for loop. If character is uppercase, convert to lowercase. If character is lowercase, convert to uppercase

--- reverse_string_case.py
+++ reverse_string_case.py
@@ -6,6 +6,12 @@ string_input = input("Enter a string: ")
 reverse_string_case = ""
 
 # using for loop, loop through each characters in the string
-#   if character is uppercase, convert to lowercase
-#   if character is lowercase, convert to uppercase
-# print swapped casing of the string input
+for character in string_input:
+    if 'A' <= character <= 'Z':  # if character is uppercase, convert to lowercase
+        reverse_string_case += chr(ord(character) + 32)
+    elif 'a' <= character <= 'z':  # if character is lowercase, convert to uppercase
+        reverse_string_case += chr(ord(character) - 32)
+    else: 
+        reverse_string_case += character
+        
+# print swapped casing of the string input 
