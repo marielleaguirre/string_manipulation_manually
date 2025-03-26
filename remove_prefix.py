4e2f9071fffe467a7a6c86a29e0b3369f055b204
marielleaguirre <mariellemaebaguirre@iskolarngbayan.pup.edu.ph>
2025-03-27 05:45:37 +0800

    Added a pseudocode for removing of prefix without using removeprefix() function

---
+++ remove_prefix.py
@@ -0,0 +1,7 @@
+# removeprefix() remove the characters at the beginning of the string that matches the function parameter. 
+# Create a program that do the same functionality without using removeprefix() function.
+
+# ask user for string input and what prefix they want to remove
+# check if the string starts with the user input prefix
+# if it does, remove the prefix by slicing the string
+# print the string input without the given prefix
