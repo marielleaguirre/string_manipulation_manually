f0b35affa095e90b2e60dc5ced4c25dc4680fa6d
marielleaguirre <mariellemaebaguirre@iskolarngbayan.pup.edu.ph>
2025-03-27 07:32:42 +0800

    Asked user to input a string

--- capitalize_first_character.py
+++ capitalize_first_character.py
@@ -2,5 +2,7 @@
 # Create a program that do the same functionality without using capitalize() function.
 
 # ask user for string input
+string_input = input("Enter a string: ")
+
 # check if string is not empty, then convert the first character to uppercase, while the rest remain lowercase
 # print the string in capitalize string format
