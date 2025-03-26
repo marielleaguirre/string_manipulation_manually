25b124e50d9e75a9016b95a60121926bc38561bb
marielleaguirre <mariellemaebaguirre@iskolarngbayan.pup.edu.ph>
2025-03-27 05:28:36 +0800

    Asked user to input a string with leading spaces

--- remove_leading_spaces.py
+++ remove_leading_spaces.py
@@ -2,6 +2,8 @@
 # Create a program that do the same functionality without using lstrip() function.
 
 # ask user for string input
+string_input = input("Enter a string with leading spaces: ")
+
 # initialize an empty result string, string_without_leading_spaces
 # using for loop, loop through each character in the string
 # if non-space character is found, start adding characters to result string
