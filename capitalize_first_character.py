1220c7a2e07f635212df5caca18fc43940b5faa6
marielleaguirre <mariellemaebaguirre@iskolarngbayan.pup.edu.ph>
2025-03-27 07:30:50 +0800

    Added a pseudocode for capitalizing the first character of the string without using capitalize() function

---
+++ capitalize_first_character.py
@@ -0,0 +1,6 @@
+# capitalize() makes the first letter of the string, capital letter. And all other letter in small case. 
+# Create a program that do the same functionality without using capitalize() function.
+
+# ask user for string input
+# check if string is not empty, then convert the first character to uppercase, while the rest remain lowercase
+# print the string in capitalize string format
