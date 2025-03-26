1f3a5e6ab0ec824fa6daf1be452aed4f0d08a471
marielleaguirre <mariellemaebaguirre@iskolarngbayan.pup.edu.ph>
2025-03-27 07:44:06 +0800

    Added a pseudocode for converting the string to titlecase without using title() function

---
+++ convert_to_title_case.py
@@ -0,0 +1,8 @@
+# title() makes all first letter of each word in the string, capital letter. And all other letter in small case. 
+# Create a program that do the same functionality without using title() function.
+
+# ask user for string input
+# split the string into words
+# for each word, convert the first character to uppercase, while the rest of characters in the word remain lowercase
+# join word into a string
+# print the string in title case
