56c9bf383189252adef92350809bc4e52bf91515
marielleaguirre <mariellemaebaguirre@iskolarngbayan.pup.edu.ph>
2025-03-27 05:27:02 +0800

    Added the pseudocode for the code of removing leading spaces without using lstrip() function

---
+++ remove_leading_spaces.py
@@ -0,0 +1,8 @@
+# lstrip() remove the space characters at the beginning of the string. 
+# Create a program that do the same functionality without using lstrip() function.
+
+# ask user for string input
+# initialize an empty result string, string_without_leading_spaces
+# using for loop, loop through each character in the string
+# if non-space character is found, start adding characters to result string
+# print the string input without leading spaces
