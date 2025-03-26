81dd25a0dd73f6824dd8528d6290a73f8f9543f3
marielleaguirre <mariellemaebaguirre@iskolarngbayan.pup.edu.ph>
2025-03-27 06:42:26 +0800

    Compare the end of the string with the suffix that the user input

--- check_string_suffix.py
+++ check_string_suffix.py
@@ -6,4 +6,9 @@ string_input = input("Enter a string: ")
 suffix_to_check = input("Enter the suffix you wish to check: ")
 
 # compare the end of the string input with the suffix input
+if string_input[-len(suffix_to_check):] == suffix_to_check:
+    is_character_suffix = True
+else:
+    is_character_suffix = False
+     
 # print True if string ends with the suffix input, otherwise print False
