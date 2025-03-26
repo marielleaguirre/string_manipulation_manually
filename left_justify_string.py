5f0d1cb251fd26a9fdb1b706a43ae58571c0b10d
marielleaguirre <mariellemaebaguirre@iskolarngbayan.pup.edu.ph>
2025-03-27 06:51:56 +0800

    Added a pseudocode for adding space/s at the end of the string without using ljust() function

---
+++ left_justify_string.py
@@ -0,0 +1,6 @@
+# ljust() add space characters at the end of the string to complete the number of characters specifies in function parameter. 
+# Create a program that do the same functionality without using ljust() function.
+
+# ask user for string input and how long they want their string to be
+# if the string input is shorter than their desired length, add spaces to the right
+# print the string with added spaces to the right
