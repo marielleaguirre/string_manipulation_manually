3c871d1cba921fd91167a1fe5f2036f7a886d7c7
marielleaguirre <mariellemaebaguirre@iskolarngbayan.pup.edu.ph>
2025-03-27 07:05:37 +0800

    Added a pseudocode for aligning the string to the center with added spaces on both sides without using center() function

---
+++ center_align_string.py
@@ -0,0 +1,7 @@
+# center() add space characters at the beginning and at the end of the string to print the string at the center. 
+# Create a program that do the same functionality without using center() function.
+
+# ask user for string input and how long they want their string to be
+# if the string input is shorter than their desired length
+#   calculate the total spaces needed, divide equally to both sides, then add space/s to both sides
+# print center align string
