fe67b81dc6317abf6e734bed0f311614c222b405
marielleaguirre <mariellemaebaguirre@iskolarngbayan.pup.edu.ph>
2025-03-27 07:18:14 +0800

    Added a pseudocode for reversing/ swapping the case of characters from uppercase to lowercase and vice versa

---
+++ reverse_string_case.py
@@ -0,0 +1,8 @@
+# swapcase() reverse the casing of each of the character of the string. 
+# Create a program that do the same functionality without using swapcase() function.
+
+# ask user for string input and initialize an empty result string, reverse_string_case
+# using for loop, loop through each characters in the string
+#   if character is uppercase, convert to lowercase
+#   if character is lowercase, convert to uppercase
+# print swapped casing of the string input
