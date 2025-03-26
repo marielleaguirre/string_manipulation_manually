61a9c05330d48695d24a686758eae4e5a15332f2
marielleaguirre <mariellemaebaguirre@iskolarngbayan.pup.edu.ph>
2025-03-27 05:31:07 +0800

    Initialized an empty result string, string_without_leading_spaces. And non_space_character to 0 (False)

--- remove_leading_spaces.py
+++ remove_leading_spaces.py
@@ -5,6 +5,9 @@
 string_input = input("Enter a string with leading spaces: ")
 
 # initialize an empty result string, string_without_leading_spaces
+string_without_leading_spaces = ""
+non_space_character = 0
+
 # using for loop, loop through each character in the string
 # if non-space character is found, start adding characters to result string
 # print the string input without leading spaces
