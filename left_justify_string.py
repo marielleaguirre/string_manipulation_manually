b21a65327e72393b0180fb8e47cf09171b3c55ee
marielleaguirre <mariellemaebaguirre@iskolarngbayan.pup.edu.ph>
2025-03-27 06:54:37 +0800

    Asked user for string input and the length of the string that they want

--- left_justify_string.py
+++ left_justify_string.py
@@ -2,5 +2,8 @@
 # Create a program that do the same functionality without using ljust() function.
 
 # ask user for string input and how long they want their string to be
+string_input = input("Enter a string: ")
+desired_length = int(input("Enter the total length you want: "))
+
 # if the string input is shorter than their desired length, add spaces to the right
 # print the string with added spaces to the right
