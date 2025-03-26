7cd0aca15f123c66613eec981f4e5840ef00d29c
marielleaguirre <mariellemaebaguirre@iskolarngbayan.pup.edu.ph>
2025-03-27 06:19:15 +0800

    Asked user for string input and initialized is_character_uppercase to True

--- check_all_uppercase.py
+++ check_all_uppercase.py
@@ -2,6 +2,9 @@
 # Create a program that do the same functionality without using isupper() function.
 
 # ask user for string input and initialize is_character_uppercase to True
+string_input = input("Enter a string: ")
+is_character_uppercase = True
+
 # using for loop, loop through each characters in the string
 # if at least one character is in lowercase, print False
 # if all characters in the string are uppercase, print True
