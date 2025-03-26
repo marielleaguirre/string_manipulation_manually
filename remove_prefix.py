d4722269b323129d9c831c3cb275ad9462631a89
marielleaguirre <mariellemaebaguirre@iskolarngbayan.pup.edu.ph>
2025-03-27 05:48:11 +0800

    Asked the user for string input and the prefix they want to remove

--- remove_prefix.py
+++ remove_prefix.py
@@ -2,6 +2,9 @@
 # Create a program that do the same functionality without using removeprefix() function.
 
 # ask user for string input and what prefix they want to remove
+string_input = input("Enter a string: ")
+remove_prefix = input("Enter the prefix you wish to remove: ")
+
 # check if the string starts with the user input prefix
 # if it does, remove the prefix by slicing the string
 # print the string input without the given prefix
