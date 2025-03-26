07de8d0b18b4af6d37c5f8adb86c4283c8cbb430
marielleaguirre <mariellemaebaguirre@iskolarngbayan.pup.edu.ph>
2025-03-27 06:06:00 +0800

    Added the pseudocode for converting the string to lowercase without using lower() function

---
+++ convert_to_lowercase.py
@@ -0,0 +1,7 @@
+# lower() converts all characters of the string into lower case. 
+# Create a program that do the same functionality without using lower() function.
+
+# ask user for string input and initialize an empty string, lower_case_string
+# using for loop to loop through each characters in the string
+# if a character is an uppercase character, convert to lowercase by adding 32 to its ASCII value
+# print the string input in lowercase
