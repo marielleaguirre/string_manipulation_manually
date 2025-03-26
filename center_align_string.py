27fbe1cd043634985c4ae2a433326283e2cb2628
marielleaguirre <mariellemaebaguirre@iskolarngbayan.pup.edu.ph>
2025-03-27 07:07:33 +0800

    Asked user for string input and their desired length of it

--- center_align_string.py
+++ center_align_string.py
@@ -2,6 +2,9 @@
 # Create a program that do the same functionality without using center() function.
 
 # ask user for string input and how long they want their string to be
+string_input = input("Enter a string: ")
+desired_length = int(input("Enter the total length you want: "))
+
 # if the string input is shorter than their desired length
 #   calculate the total spaces needed, divide equally to both sides, then add space/s to both sides
 # print center align string
