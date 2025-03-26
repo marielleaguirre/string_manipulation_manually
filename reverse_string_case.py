743037b80e30cb20f8054a7e7b3a824dd7f40ed9
marielleaguirre <mariellemaebaguirre@iskolarngbayan.pup.edu.ph>
2025-03-27 07:19:57 +0800

    Asked user to input a string and initialized an empty string, reverse_string_case

--- reverse_string_case.py
+++ reverse_string_case.py
@@ -2,6 +2,9 @@
 # Create a program that do the same functionality without using swapcase() function.
 
 # ask user for string input and initialize an empty result string, reverse_string_case
+string_input = input("Enter a string: ")
+reverse_string_case = ""
+
 # using for loop, loop through each characters in the string
 #   if character is uppercase, convert to lowercase
 #   if character is lowercase, convert to uppercase
