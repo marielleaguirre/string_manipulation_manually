2b11f89008cd15f0402fae6fdbf21f59b5b0dc9e
marielleaguirre <mariellemaebaguirre@iskolarngbayan.pup.edu.ph>
2025-03-27 06:58:23 +0800

    If the string is shorter than desired length add space/s to the right to satisfy the value of the variable desired_length

--- left_justify_string.py
+++ left_justify_string.py
@@ -6,4 +6,7 @@ string_input = input("Enter a string: ")
 desired_length = int(input("Enter the total length you want: "))
 
 # if the string input is shorter than their desired length, add spaces to the right
+if len(string_input) < desired_length:
+    string_input = string_input + " " * (desired_length - len(string_input))
+
 # print the string with added spaces to the right
