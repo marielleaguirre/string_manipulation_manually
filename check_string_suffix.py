cd7552b051d5be1acc9afc06b76922db1e4decaf
marielleaguirre <mariellemaebaguirre@iskolarngbayan.pup.edu.ph>
2025-03-27 06:38:37 +0800

    Added a pseudocode for checking the suffix in the string withput using endswith() function

---
+++ check_string_suffix.py
@@ -0,0 +1,6 @@
+# endswith() check if the string end part matches the function parameter. 
+# Create a program that do the same functionality without using endswith() function.
+
+# ask user for string input and a suffix to check
+# compare the end of the string input with the suffix input
+# print True if string ends with the suffix input, otherwise print False
