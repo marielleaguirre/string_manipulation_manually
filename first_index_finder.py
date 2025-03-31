7e87a6a9fc6c83c833487dfb40b1169627690872
marielleaguirre <mariellemaebaguirre@iskolarngbayan.pup.edu.ph>
2025-03-31 19:20:30 +0800

    Asked the user to input a string and the word/substring they want to find

--- first_index_finder.py
+++ first_index_finder.py
@@ -2,6 +2,9 @@
 # Create a program that do the same functionality without using index() function.
 
 # ask user for string input and the word/substring to find
+string_input = input("Enter a string: ")
+search_word = input("Enter the word/substring to find: ")
+
 # use for loop to loop through the string to find the first occurrence
 # if a match is found, print the index and break the loop
 # if no match is found after the loop, print a message saying it was not found
