9c56835b77d6baa87596777713342d35a8327bf4
marielleaguirre <mariellemaebaguirre@iskolarngbayan.pup.edu.ph>
2025-03-31 17:58:46 +0800

    Added a pseudocode for removing the suffix input in the string input

---
+++ remove_suffix.py
@@ -0,0 +1,7 @@
+# removesuffix() remove the characters at the end of the string that matches the function parameter. 
+# Create a program that do the same functionality without using removesuffix() function.
+
+# ask user for a string input and what suffix they want to remove
+# check if the string ends with the suffix input
+# remove the suffix manually by slicing
+# print the string without the given suffix
