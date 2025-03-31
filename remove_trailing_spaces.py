c1b3d06e12bb39bfe75497f3a7e76a4ba37790aa
marielleaguirre <mariellemaebaguirre@iskolarngbayan.pup.edu.ph>
2025-03-31 17:51:28 +0800

    Sliced the string to exclude trailing spaces

--- remove_trailing_spaces.py
+++ remove_trailing_spaces.py
@@ -9,6 +9,11 @@ index = len(string_input) -1
 
 # loop backward through the string until a non-space character is found
 for character in range(len(string_input) - 1, -1, -1):
-    
-# slice the string to exclude trailing spaces
+    # slice the string to exclude trailing spaces
+    if string_input[character] != " ":
+        index = character
+        break
+
+string_without_trailing_spaces = string_input[:index + 1]
+
 # print the modified string
