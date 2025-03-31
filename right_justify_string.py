10f350e22eb031f44b35985edcf1a292599db343
marielleaguirre <mariellemaebaguirre@iskolarngbayan.pup.edu.ph>
2025-03-31 18:40:52 +0800

    Added a pseudocode for adding space/s at the beginning of the string, right justify

---
+++ right_justify_string.py
@@ -0,0 +1,6 @@
+# rjust() add space characters at the beginning of the string to complete the number of characters specifies in function parameter. 
+# Create a program that do the same functionality without using rjust() function.
+
+# ask user for string input and how long they want their string to be
+# if the string input is shorter than their desired length, add spaces to the left
+# print the string with added spaces at the beginning
