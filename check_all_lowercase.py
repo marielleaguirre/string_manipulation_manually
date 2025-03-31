53415cf6034d026a6850c628d5c00452651a8fbe
marielleaguirre <mariellemaebaguirre@iskolarngbayan.pup.edu.ph>
2025-03-31 18:20:31 +0800

    Added a pseudocode for checking if all characters are in lowercase

---
+++ check_all_lowercase.py
@@ -0,0 +1,7 @@
+# islower() check if all characters of the string is on lower case. 
+# Create a program that do the same functionality without using islower() function.
+
+# ask user for string input and initialize is_character_lowercase to True
+# using for loop, loop thorugh each characters in the string
+# if at least one character is in uppercase, print False
+# if all characters in the string are in lowercase, print True
