024b599ffd0a6907fa98ba1c7a066ec1cf4eab41
marielleaguirre <mariellemaebaguirre@iskolarngbayan.pup.edu.ph>
2025-03-31 18:10:16 +0800

    Added a pseudocode for converting the string to uppercase

---
+++ convert_to_uppercase.py
@@ -0,0 +1,7 @@
+# upper() converts all characters of the string into upper case. 
+# Create a program that do the same functionality without using upper() function.
+
+# ask user for string input and initialize an empty string, upper_case_string
+# using for loop, loop through each characters in the string
+# if a character is a lowercase character, convert to uppercase by subtracting 32 to its ASCII value
+# print the string input in uppercase
