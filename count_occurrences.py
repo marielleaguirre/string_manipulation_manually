a7e72eb3eaeff0271e4fe3001a0ba2a3761e8bd7
marielleaguirre <mariellemaebaguirre@iskolarngbayan.pup.edu.ph>
2025-03-31 19:05:27 +0800

    Initialize both the counter and index to 0

--- count_occurrences.py
+++ count_occurrences.py
@@ -6,6 +6,9 @@ string_input = input("Enter a string: ")
 search_word = input("Enter the word/substring to count: ")
 
 # initialize counter and index
+occurrence_count = 0
+index = 0
+
 # loop to count occurrences, allowing overlaps
 # move forward by 1 to check overlapping occurrences
 # print the result
