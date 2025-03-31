c872ad78442592daf77901c6fd155c75e3b6f1b7
marielleaguirre <mariellemaebaguirre@iskolarngbayan.pup.edu.ph>
2025-03-31 17:32:34 +0800

    Asked user for a string input

--- remove_trailing_spaces.py
+++ remove_trailing_spaces.py
@@ -2,6 +2,8 @@
 # Create a program that do the same functionality without using rstrip() function.
 
 # ask user for string input
+string_input = input("Enter a string with trailing spaces: ")
+
 # initialize an index variable at the last position of the string
 # loop backward through the string until a non-space character is found
 # slice the string to exclude trailing spaces
