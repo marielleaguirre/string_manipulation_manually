2b8da6428ec94c594f05fa01190639f1599323f1
marielleaguirre <mariellemaebaguirre@iskolarngbayan.pup.edu.ph>
2025-03-31 19:31:00 +0800

    Asked the user to input a string and the word/substring they want to find

--- last_index_finder.py
+++ last_index_finder.py
@@ -2,6 +2,9 @@
 # Create a program that do the same functionality without using rindex() function.
 
 # ask user for string input and the word/substring to find
+string_input = input("Enter a string: ")
+search_word = input("Enter the word/substring to find: ")
+
 # use for loop to loop through the string in reverse to find the last occurrence
 # if a match is found, print the index and break the loop
 # if no match is found after the loop, print a message saying it was not found
