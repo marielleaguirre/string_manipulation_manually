6611df919d419ef65e0fa46b487bff02909f9332
marielleaguirre <mariellemaebaguirre@iskolarngbayan.pup.edu.ph>
2025-03-31 18:56:09 +0800

    If the string input is shorter than desired length, add zeros to the left

--- add_leading_zeros.py
+++ add_leading_zeros.py
@@ -6,4 +6,7 @@ string_input = input("Enter a string: ")
 desired_length = int(input("Enter the total length you want: "))
 
 # if the string input is shorter than desired length, add zeros to the left 
+if len(string_input) < desired_length:
+    string_input = "0" * (desired_length - len(string_input)) + string_input
+    
 # print the modified string with added zeros to the left
