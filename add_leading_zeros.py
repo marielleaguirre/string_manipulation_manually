b1a88ac6558d2ef343ca32b11634b58fce81ae19
marielleaguirre <mariellemaebaguirre@iskolarngbayan.pup.edu.ph>
2025-03-31 18:57:28 +0800

    Print the modified string with added zeros to the left

--- add_leading_zeros.py
+++ add_leading_zeros.py
@@ -8,5 +8,6 @@ desired_length = int(input("Enter the total length you want: "))
 # if the string input is shorter than desired length, add zeros to the left 
 if len(string_input) < desired_length:
     string_input = "0" * (desired_length - len(string_input)) + string_input
-    
+
 # print the modified string with added zeros to the left
+print(f"Modified string: '{string_input}'")
