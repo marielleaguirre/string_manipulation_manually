5c23787b302c3c7f40407f5724cffc53f69d18aa
marielleaguirre <mariellemaebaguirre@iskolarngbayan.pup.edu.ph>
2025-03-31 18:31:31 +0800

    Added a pseudocode for checking if the string starts with the prefix input

---
+++ check_string_prefix.py
@@ -0,0 +1,6 @@
+# startswith() check if the string beginning part matches the function parameter. 
+# Create a program that do the same functionality without using startswith() function.
+
+# ask user for string input and prefix they want to check
+# compare the beginning of the string input with the prefix input
+# print True if the string starts with the prefix input, otherwise print False
