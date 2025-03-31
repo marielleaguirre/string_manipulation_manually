e43783841616e7617f3bc696a112f19b716d1e19
marielleaguirre <mariellemaebaguirre@iskolarngbayan.pup.edu.ph>
2025-03-31 19:29:58 +0800

    Added a pseudocode for finding the last occurrence of the word/substring

---
+++ last_index_finder.py
@@ -0,0 +1,7 @@
+# rindex() return the first location of the function parameter in the string starting from the last character. 
+# Create a program that do the same functionality without using rindex() function.
+
+# ask user for string input and the word/substring to find
+# use for loop to loop through the string in reverse to find the last occurrence
+# if a match is found, print the index and break the loop
+# if no match is found after the loop, print a message saying it was not found
