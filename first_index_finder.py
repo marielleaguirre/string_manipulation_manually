06786c9086faa3213405c10b4a3881c827382e8a
marielleaguirre <mariellemaebaguirre@iskolarngbayan.pup.edu.ph>
2025-03-31 19:16:57 +0800

    Added a pseudocode for finding the first occurrence of the word/substring

---
+++ first_index_finder.py
@@ -0,0 +1,7 @@
+# index() return the first location of the function parameter in the string. 
+# Create a program that do the same functionality without using index() function.
+
+# ask user for string input and the word/substring to find
+# use for loop to loop through the string to find the first occurrence
+# if a match is found, print the index and break the loop
+# if no match is found after the loop, print a message saying it was not found
