d21bfba0e2e47e48ca42f28e8cca88a11162806b
marielleaguirre <mariellemaebaguirre@iskolarngbayan.pup.edu.ph>
2025-03-31 19:02:34 +0800

    Added a pseudocode for counting the total of occurrences of the word/substring

---
+++ count_occurrences.py
@@ -0,0 +1,8 @@
+# count() return how many time the function parameter appear in the string. 
+# Create a program that do the same functionality without using count() function.
+
+# ask user for string input and word/substring to count
+# initialize counter and index
+# loop to count occurrences, allowing overlaps
+# move forward by 1 to check overlapping occurrences
+# print the result
