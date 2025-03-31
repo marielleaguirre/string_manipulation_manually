188cd3cd7ad72e86e86c925011ac170121a7b0a4
marielleaguirre <mariellemaebaguirre@iskolarngbayan.pup.edu.ph>
2025-03-31 18:51:11 +0800

    Added a pseudocode for adding zeros to fill the beginning of the string

---
+++ add_leading_zeros.py
@@ -0,0 +1,6 @@
+# zfill() add zero characters at the beginning of the string to complete the number of characters specifies in function parameter. 
+# Create a program that do the same functionality without using zfill() function.
+
+# ask user for string input and the total length they want
+# if the string input is shorter than desired length, add zeros to the left 
+# print the modified string with added zeros to the left
