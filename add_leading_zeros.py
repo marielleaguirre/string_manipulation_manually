066be18c46740d12a91255e88f418eda3c1d17cb
marielleaguirre <mariellemaebaguirre@iskolarngbayan.pup.edu.ph>
2025-03-31 18:54:11 +0800

    Asked user for string input and the total length they want

--- add_leading_zeros.py
+++ add_leading_zeros.py
@@ -2,5 +2,8 @@
 # Create a program that do the same functionality without using zfill() function.
 
 # ask user for string input and the total length they want
+string_input = input("Enter a string: ")
+desired_length = int(input("Enter the total length you want: "))
+
 # if the string input is shorter than desired length, add zeros to the left 
 # print the modified string with added zeros to the left
