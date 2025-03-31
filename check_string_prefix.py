497b3b43bc6dcb93356ae2a791d07437a3e9df11
marielleaguirre <mariellemaebaguirre@iskolarngbayan.pup.edu.ph>
2025-03-31 18:33:18 +0800

    Asked user to input a string and the prefix they want to check

--- check_string_prefix.py
+++ check_string_prefix.py
@@ -2,5 +2,8 @@
 # Create a program that do the same functionality without using startswith() function.
 
 # ask user for string input and prefix they want to check
+string_input = input("Enter a string: ")
+prefix_to_check = input("Enter the prefix you wish to check: ")
+
 # compare the beginning of the string input with the prefix input
 # print True if the string starts with the prefix input, otherwise print False
