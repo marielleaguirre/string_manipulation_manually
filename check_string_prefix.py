b24cbbb43e5064a5dd786cf1b8dd309b569b9ee5
marielleaguirre <mariellemaebaguirre@iskolarngbayan.pup.edu.ph>
2025-03-31 18:35:18 +0800

    Compare the beginning of the string input with the prefix input

--- check_string_prefix.py
+++ check_string_prefix.py
@@ -6,4 +6,9 @@ string_input = input("Enter a string: ")
 prefix_to_check = input("Enter the prefix you wish to check: ")
 
 # compare the beginning of the string input with the prefix input
+if string_input[:len(prefix_to_check)] == prefix_to_check:
+    is_character_prefix = True
+else:
+    is_character_prefix = False
+    
 # print True if the string starts with the prefix input, otherwise print False
